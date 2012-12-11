81efd348c8ffd6286cbf024237252e4a005376e4
Jonathan Dixon <dixonj16@gmail.com>
2012-12-10 22:49:48 -0500

    Consistent use of single quotes, and test organization.

--- test/ocr.js
+++ test/ocr.js
@@ -1,19 +1,9 @@
 var assert = require('assert'),
     canvas = require('canvas'),
     _ = require('underscore'),
-    processor = require('../lib/processor'),
     horus = require('../lib/horus');
 
 describe('OCR', function(){
-
-  describe('dataFromStringTest', function(){
-    it('test extract data from character', function(){
-      var size = 10, character = 'A';
-      var result = processor.dataFromString(character, size, 'Helvetica');
-      assert.ok(result.input.length == size * size);
-      assert.ok(result.output[character] == 1);
-    })
-  })
 
   describe('autoTrainAsciiTest', function(){
     it('train network to recognize visible ascii characters', function(){
@@ -24,6 +14,20 @@ describe('OCR', function(){
       });
 
       assert.ok(result.error < .01 || result.iterations == 100);
+    })
+  }),
+
+  describe('runOnFileTest', function() {
+    it('should return the text content of the file', function() {
+      var ocr = new horus.OCR();
+      var result = ocr.autoTrainAscii({
+        errorThresh: 0.01,
+        iterations: 100
+      });
+
+      result = ocr.runOnFile('./test/resources/test_01.jpg', function(results) {
+        assert.ok(result);
+      });
     })
   })
 
--- test/processor.js
+++ test/processor.js
@@ -1,15 +1,14 @@
 var assert = require('assert'),
     canvas = require('canvas'),
     _ = require('underscore'),
-    processor = require('../lib/processor'),
-    horus = require('../lib/horus');
+    processor = require('../lib/processor');
 
 describe('Processor', function(){
 
   describe('extractDataTest', function(){
     it('test extract data from canvas image data', function(){
       var size = 10;
-      var context = new canvas(size).getContext("2d");
+      var context = new canvas(size).getContext('2d');
       context.font = size + 'px Helvetica';
       context.fillText('A', 0, size);
       
@@ -20,6 +19,15 @@ describe('Processor', function(){
 
       var sum = _.reduce(result, function(sum, n) { return sum + n; }, 0);
       assert.ok(sum > 1, 'Sum: ' + sum + ', was expecting > 1.');
+    })
+  }),
+
+  describe('dataFromStringTest', function(){
+    it('test extract data from character', function(){
+      var size = 10, character = 'A';
+      var result = processor.dataFromString(character, size, 'Helvetica');
+      assert.ok(result.input.length == size * size);
+      assert.ok(result.output[character] == 1);
     })
   })
 
